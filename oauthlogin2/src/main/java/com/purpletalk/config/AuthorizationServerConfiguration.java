368fb56c7da9399d4c4758acb4adcce9ffeb4ea5
techsravanit <38765158+techsravanit@users.noreply.github.com>
2018-06-13 14:28:06 +0530

    Add files via upload

--- oauthlogin2/src/main/java/com/purpletalk/config/AuthorizationServerConfiguration.java
+++ oauthlogin2/src/main/java/com/purpletalk/config/AuthorizationServerConfiguration.java
@@ -12,22 +12,41 @@ import org.springframework.security.oauth2.config.annotation.web.configurers.Aut
 import org.springframework.security.oauth2.provider.approval.UserApprovalHandler;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 
+/**
+ * Convenient strategy for configuring an OAUth2 Authorization Server. 
+ * Beans of this type are applied to the Spring context automatically if you use @EnableAuthorizationServer.
+ */
 @Configuration
 @EnableAuthorizationServer
 public class AuthorizationServerConfiguration extends AuthorizationServerConfigurerAdapter {
  
     private static String REALM="MY_OAUTH_REALM";
-     
+    
+    /**
+     *  for OAuth2 tokens.
+     */
     @Autowired
     private TokenStore tokenStore;
  
+    /**
+     * It checks whether a given client authentication request has been approved by the current user.
+     */
     @Autowired
     private UserApprovalHandler userApprovalHandler;
  
+    /**
+     * Processes an Authentication request.
+     */
     @Autowired
     @Qualifier("authenticationManagerBean")
     private AuthenticationManager authenticationManager;
- 
+    
+    /**
+     * Configure the ClientDetailsService, e.g. declaring individual clients and their properties. 
+     * Note that password grant is not enabled (even if some clients are allowed it) unless an AuthenticationManager is
+	 * supplied to the #configure(AuthorizationServerEndpointsConfigurer). 
+	 * At least one client, or a fully formed custom ClientDetailsService must be declared or the server will not start.
+     */
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
  
@@ -40,12 +59,24 @@ public class AuthorizationServerConfiguration extends AuthorizationServerConfigu
             .accessTokenValiditySeconds(120).//Access token is only valid for 2 minutes.
             refreshTokenValiditySeconds(600);//Refresh token is only valid for 10 minutes.
     }
- 
+    
+    /**
+     * Configure the non-security features of the Authorization Server endpoints, like token store, token
+	 * customizations, user approvals and grant types. 
+	 * You shouldn't need to do anything by default, unless you need password grants, 
+	 * in which case you need to provide an AuthenticationManager.
+     */
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
         endpoints.tokenStore(tokenStore).userApprovalHandler(userApprovalHandler)
                 .authenticationManager(authenticationManager);
     }
+    
+    /**
+     * Configure the security of the Authorization Server, which means in practical terms the /oauth/token endpoint.
+     * he default settings cover the most common requirements, following recommendations from the OAuth2 spec, 
+     * so you don't need to do anything here to get a basic server up and running.
+     */
  
     @Override
     public void configure(AuthorizationServerSecurityConfigurer oauthServer) throws Exception {
--- oauthlogin2/src/main/java/com/purpletalk/config/OAuth2SecurityConfiguration.java
+++ oauthlogin2/src/main/java/com/purpletalk/config/OAuth2SecurityConfiguration.java
@@ -6,12 +6,14 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.oauth2.provider.ClientDetailsService;
+import org.springframework.security.oauth2.provider.approval.Approval;
 import org.springframework.security.oauth2.provider.approval.ApprovalStore;
 import org.springframework.security.oauth2.provider.approval.TokenApprovalStore;
 import org.springframework.security.oauth2.provider.approval.TokenStoreUserApprovalHandler;
@@ -19,71 +21,126 @@ import org.springframework.security.oauth2.provider.request.DefaultOAuth2Request
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
- 
+
+/**
+ * Allows customization to the WebSecurity. 
+ * In most instances users will use @EnableWebSecurity and a create Configuration 
+ * that extends WebSecurityConfigurerAdapter. 
+ */
 @Configuration
 @EnableWebSecurity
 public class OAuth2SecurityConfiguration extends WebSecurityConfigurerAdapter {
- 
-    @Autowired
-    private ClientDetailsService clientDetailsService;
-     
-    @Autowired
-    public void globalUserDetails(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-        .withUser("admin").password("admin").roles("ADMIN").and()
-        .withUser("user").password("user").roles("USER");
-    }
- 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-        .csrf().disable()
-        .anonymous().disable()
-        .authorizeRequests()
-        .antMatchers("/oauth/token").permitAll();
-    }
- 
-    @Override
-    @Bean
-    public AuthenticationManager authenticationManagerBean() throws Exception {
-        return super.authenticationManagerBean();
-    }
- 
- 
-    @Bean
-    public TokenStore tokenStore() {
-        return new InMemoryTokenStore();
-    }
- 
-    @Bean
-    @Autowired
-    public TokenStoreUserApprovalHandler userApprovalHandler(TokenStore tokenStore){
-        TokenStoreUserApprovalHandler handler = new TokenStoreUserApprovalHandler();
-        handler.setTokenStore(tokenStore);
-        handler.setRequestFactory(new DefaultOAuth2RequestFactory(clientDetailsService));
-        handler.setClientDetailsService(clientDetailsService);
-        return handler;
-    }
-     
-    @Bean
+
+	/**
+	 * A service that provides the details about an OAuth2 client.
+	 */
+	@Autowired
+	private ClientDetailsService clientDetailsService;
+
+	/**
+	 * Used by the default implementation of #authenticationManager() to attempt to obtain an AuthenticationManager. 
+	 * If overridden, the AuthenticationManagerBuilder should be used to specify the AuthenticationManager.
+	 * The #authenticationManagerBean() method can be used to expose the resulting AuthenticationManager as a Bean.
+	 * The #userDetailsServiceBean() can be used to expose the last populated UserDetailsService that is created
+	 * with the AuthenticationManagerBuilder as a Bean. 
+	 * The UserDetailsService will also automatically be populated on HttpSecurity#getSharedObject(Class)
+	 * for use with other SecurityContextConfigurer} (i.e. RememberMeConfigurer )
+	 * 
+	 * @param auth
+	 * @throws Exception
+	 */
+	@Autowired
+	public void globalUserDetails(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication()
+		.withUser("admin").password("admin").roles("ADMIN").and()
+		.withUser("user").password("user").roles("USER");
+	}
+
+	/**
+	 * Override this method to configure the HttpSecurity. 
+	 * Typically subclasses should not invoke this method by calling super as it may override their configuration.
+	 * The default configuration is:
+	 *	 http.authorizeRequests().anyRequest().authenticated().and().formLogin().and().httpBasic(); 
+	 */
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+		.csrf().disable()
+		.anonymous().disable()
+		.authorizeRequests()
+		.antMatchers("/oauth/token").permitAll();
+	}
+
+	/**
+	 * Override this method to expose the AuthenticationManager from
+	 * #configure(AuthenticationManagerBuilder) to be exposed as a Bean.
+	 */
+	@Override
+	@Bean
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+		return super.authenticationManagerBean();
+	}
+
+	/**
+	 * InMemoryTokenStore -- Implementation of token services that stores tokens in memory.
+	 * @return
+	 */
+	@Bean
+	public TokenStore tokenStore() {
+		return new InMemoryTokenStore();
+	}
+
+	/**
+	 * TokenStoreUserApprovalHandler -- A user approval handler that remembers approval decisions by consulting existing tokens.
+	 * setClientDetailsService -- Service to load client details (optional) for auto approval checks.
+	 * @param tokenStore
+	 * @return
+	 */
+	@Bean
+	@Autowired
+	public TokenStoreUserApprovalHandler userApprovalHandler(TokenStore tokenStore){
+		TokenStoreUserApprovalHandler handler = new TokenStoreUserApprovalHandler();
+		handler.setTokenStore(tokenStore);
+		handler.setRequestFactory(new DefaultOAuth2RequestFactory(clientDetailsService));
+		handler.setClientDetailsService(clientDetailsService);
+		return handler;
+	}
+
+	/* To avoid the below exception
+			There is no PasswordEncoder mapped for the id null */
+	@SuppressWarnings("deprecation")
+	@Bean
+	public static NoOpPasswordEncoder passwordEncoder() {
+		return (NoOpPasswordEncoder) NoOpPasswordEncoder.getInstance();
+	}
+
+	/**
+	 * ApprovalStore -- for saving, retrieving and revoking user approvals (per client, per scope).
+	 * TokenApprovalStore -- An ApprovalStore that works with an existing TokenStore, extracting implicit Approvals from the content of tokens already in the store. 
+	 * Useful interface so that users can list and revoke approvals even if they are not really represented in such a way internally.
+	 *
+	 * @param tokenStore
+	 * @return
+	 * @throws Exception
+	 */
+	/*  @Bean
     @Autowired
     public ApprovalStore approvalStore(TokenStore tokenStore) throws Exception {
         TokenApprovalStore store = new TokenApprovalStore();
         store.setTokenStore(tokenStore);
         return store;
-    }
-    
-    @Bean
+    }*/
+
+	/**
+	 * Allows modifying and accessing the UserDetailsService from #userDetailsServiceBean() 
+	 * without interacting with the ApplicationContext. 
+	 * Developers should override this method when changing the instance of #userDetailsServiceBean().
+	 */
+	/*  @Bean
     public UserDetailsService userDetailsService() {
         InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
         manager.createUser(User.withUsername("user").password("user").roles("USER").build());
         return manager;
-    }
-    
-    @SuppressWarnings("deprecation")
-    @Bean
-    public static NoOpPasswordEncoder passwordEncoder() {
-    return (NoOpPasswordEncoder) NoOpPasswordEncoder.getInstance();
-    }
-       
+    }*/
+
 }
